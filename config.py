d6a4344ad1facc331e26b742dc23111481e83cd1
Vinmar Tipay <vinmar.tipay@outlook.com>
2025-06-14 14:35:22 +0000

    added another test for user model. installed cryptography

--- config.py
+++ config.py
@@ -7,7 +7,9 @@ class Config:
 
 class TestConfig(Config): # New class for testing
     TESTING = True # Flask's testing flag
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' # In-memory SQLite database
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://remoteroot:remoteroot@localhost:3306/flask_simple_blog_app_test_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:' # In-memory SQLite database
     # For a persistent test database file (useful for debugging test DB state):
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
     # You might also want to disable CSRF in testing forms for easier testing:
--- tests/modules/user/test_models.py
+++ tests/modules/user/test_models.py
@@ -1,4 +1,6 @@
 import unittest
+import bcrypt
+from sqlalchemy import inspect
 from datetime import datetime, timezone, timedelta
 from app.modules.user.models.user_model import UserModel
 from app.modules.user.models.user_pid_model import UserPIDModel
@@ -9,6 +11,7 @@ from app import create_app, db
 class UserModelCase(unittest.TestCase):
 
     def setUp(self):
+
         # Create an app context with test config
         self.app = create_app(TestConfig)
         self.app_context = self.app.app_context()
@@ -21,9 +24,25 @@ class UserModelCase(unittest.TestCase):
         db.drop_all() # drop database and tables
         self.app_context.pop()
     
+    def test_user_creation(self):
+        
+        u1 = UserModel(firstname = "John", lastname = "Doe", email = "john.doe@email.com")
+        u1.set_password("cat")
+        db.session.add(u1)
+        db.session.commit()
+
+        retrieved_user1 = UserModel.query.filter_by(email="john.doe@email.com").first() or None
+        self.assertIsNotNone(retrieved_user1)
+        self.assertEqual(retrieved_user1.firstname, "John")
+        self.assertEqual(retrieved_user1.lastname, "Doe")
+        self.assertEqual(retrieved_user1.email, "john.doe@email.com")
+        self.assertTrue(retrieved_user1.check_password("cat"))
+        self.assertFalse(retrieved_user1.check_password("dog"))
+    
     def test_password_hashing(self):
         user = UserModel(firstname="john", lastname="doe", email="email@email.com")
         user.set_password("cat")
+
         self.assertFalse(user.check_password("dog")) # test for incorrect password
         self.assertTrue(user.check_password("cat")) # test for correct password
 
